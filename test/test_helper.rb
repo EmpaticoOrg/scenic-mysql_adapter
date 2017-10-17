4c030864e41c2d3ec750435cf88dc3ccc8bef7b0
Lance Ivy <lance@cainlevy.net>
2017-10-17 09:38:54 -0700

    add activerecord to tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,10 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "scenic/mysql_adapter"
 
 require "minitest/autorun"
+
+# establish the database connection
+require "active_record"
+ActiveRecord::Base.establish_connection(
+  ENV['DATABASE_URL'] || 'mysql2://root@localhost/scenic_mysql_adapter_test'
+)
+ActiveRecord::Migration.verbose = false
